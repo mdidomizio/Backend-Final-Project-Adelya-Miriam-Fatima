65fcce8bfbdc808867058f060455938603d22fba
Miriam <miriam.didomizio@googlemail.com>
2022-10-28 16:06:37 +0200

    added the routes for users.js

---
+++ routes/users.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { validUser } from "../validation/users.js";
+import { postCreateUserEntry, loginUser } from "../controllers/users.js";
+
+const router = Router();
+
+router.route("/")
+   .post(validUser, postCreateUserEntry);
+
+router.route('/login')
+   .post(loginUser)
+
+
+export default router;
