96a5d245dc2375e7d8ec51fbef42c7875a059615
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-11-18 15:06:27 +0100

    favorites setup in server and router

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import {
   createErrorMissingPath,
   errorMiddleware,
 } from "./middleware/errors.js";
-// import wardrobeRouter from "./routes/wardrobe.js";
+import wardrobeRouter from "./routes/wardrobe.js";
 import usersRouter from "./routes/users.js";
 
 //create an instance of the server:
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(corsHeaders);
 
 // request handler
-// TODO app.use("/wardrobe", wardrobeRouter);
+app.use("/favorites", favoritesRouter);
 app.use("/users", usersRouter);
 
 app.use("*", createErrorMissingPath);
