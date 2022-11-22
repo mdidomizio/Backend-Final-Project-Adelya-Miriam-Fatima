b5e67e3f34cfcf5be55a85734963363e4467f6e9
adelyanasretdinova <adelya.nasretdinova@gmail.com>
2022-11-22 13:47:28 +0100

    corrected db table favorite recepies

--- recipes.sql
+++ recipes.sql
@@ -100,7 +100,7 @@ and it is ready.',
 SELECT *
 FROM recipes
 
-CREATE TABLE favoriterecipes ( idMeal VARCHAR(20) NOT NULL,
+CREATE TABLE favoriterecipes ( idMeal VARCHAR(20) NOT NULL, strTags VARCHAR(20),
                                                   strMeal VARCHAR(200) NOT NULL,
                                                                        strCategory VARCHAR(200) NOT NULL,
                                                                                                 strArea VARCHAR(200) NOT NULL,
@@ -144,165 +144,11 @@ CREATE TABLE favoriterecipes ( idMeal VARCHAR(20) NOT NULL,
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           strMeasure18 VARCHAR(200),
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        strMeasure19 VARCHAR(200),
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     strMeasure20 VARCHAR(200),
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 strInstructions TEXT NOT NULL,
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      strMealThumb TEXT NOT NULL,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 strInstructions VARCHAR(2000000),
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      strMealThumb VARCHAR(20000),
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         userid UUID NOT NULL,
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     CONSTRAINT fk_user
                               FOREIGN KEY(userid) REFERENCES users(id) );
-
-INSERT INTO favoriterecipes (idMeal,
-strMeal,
-strCategory,
-strArea,
-strIngredient1,
-strIngredient2,
-strIngredient3,
-strIngredient4,
-strIngredient5,
-strIngredient6,
-strIngredient7,
-strIngredient8,
-strIngredient9,
-strIngredient10,
-strIngredient11,
-strIngredient12,
-strIngredient13,
-strIngredient14,
-strIngredient15,
-strIngredient16,
-strIngredient17,
-strIngredient18,
-strIngredient19,
-strIngredient20,
-strMeasure1,
-strMeasure2,
-strMeasure3,
-strMeasure4,
-strMeasure5,
-strMeasure6,
-strMeasure7,
-strMeasure8,
-strMeasure9,
-strMeasure10,
-strMeasure11,
-strMeasure12,
-strMeasure13,
-strMeasure14,
-strMeasure15,
-strMeasure16,
-strMeasure17,
-strMeasure18,
-strMeasure19,
-strMeasure20,
-strInstructions,
-strMealThumb,
-userid)
-VALUES (
-'52783',
-'Rigatoni with fennel sausage sauce',
-'Lamb',
-'Moroccan',
-'olive oil',
-'Italian fennel sausages',
-'onion',
-'fennel bulb',
-'smoky paprika',
-'garlic',
-'fennel seeds',
-'red wine',
-'chopped tomatoes',
-'caster sugar',
-'pitted black olives',
-'rigatoni',
-'pecorino',
-'anchovy fillet',
-'garlic',
-'olive oil',
-'basil leaves',
-'',
-'',
-'',
-'2½ tbsp',
-'6 cut into 1.5cm-thick slices',
-'1 large peeled and chopped',
-'1 trimmed and roughly chopped; reserve any fronds to garnish',
-'½ tsp',
-'1 clove, peeled and sliced',
-'2 tsp lightly toasted and then gently crushed',
-'100ml',
-'400g tinned',
-'½ tsp',
-'50g cut in half lengthways',
-'500g',
-'30g roughly crumbled into 0.5cm pieces',
-'1  rinsed and patted dry',
-'1 clove, peeled and crushed',
-'60ml',
-'50g torn',
-'',
-'',
-'',
-'Heat a tablespoon of oil in a large saute pan
-for which you have a lid. Add the sausage pieces
-and fry on a medium-high heat
-for 10 minutes,
-    stirring regularly,
-    until golden-brown all over. Transfer the sausages to a plate,
-                                                          then add the onion
-and fennel to the hot pan
-and fry
-for 15 minutes,
-    stirring once in a while,
-                       until soft
-and caramelised;
-
-if the pan goes a bit dry,
-                      add a teaspoon
-or so of extra oil. Stir in the paprika,
-                            garlic
-and half the fennel seeds,
-             fry
-for two minutes
-more,
-                then pour on the wine
-and boil
-for 30 seconds,
-    to reduce by half. Add the tomatoes,
-                           sugar,
-                           100ml water,
-                                 the seared sausage
-and half a teaspoon of salt,
-                       cover
-and simmer
-for 30 minutes;
-
-remove the lid after 10 minutes,
-and cook until the sauce is thick
-and rich. Remove
-from the heat,
-     stir through the olives
-and remaining fennel seeds
-and
-set aside until you’re ready to serve.\r\n\r\nBring a large pot of salted water to a boil,
-                                                                                   add the pasta
-and cook
-for 12-14 minutes (
-                   or according to the instructions on the packet),
-          until al dente. Meanwhile,
-                reheat the sauce. Drain the pasta,
-                                            return it to the pot,
-                                                         stir in a tablespoon of oil,
-                                                                                 then divide between the bowls. \r\n\r\nPut all the pesto ingredients
-except the basil in the small bowl of a food processor. Add a tablespoon of water
-and blitz to a rough paste. Add the basil,
-                                then blitz until just combined (the pesto has a much better texture if the basil is not overblended).\r\n\r\nSpoon over the ragù
-and top with a spoonful of pesto. Finish with a sprinkling of chopped fennel fronds,
-                                                                             if you have any,
-and serve at once.',
-'https://www.themealdb.com/images/media/meals/qtqvys1468573168.jpg',
-'4d49163c-0e89-40c9-b067-ff491419d972'
-)
-
 
 SELECT *
 FROM favoriterecipes;
